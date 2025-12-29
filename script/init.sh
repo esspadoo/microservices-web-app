4e536ff7c99ad0b9194d11baaf9804612c76185d
leonardo <leonardoongaro4@gmail.com>
2025-12-29 22:42:44 +0100

    small upgrades

--- script/init.sh
+++ script/init.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 source "compileProject.sh"
-source "guardianCrawler.sh"
+#source "guardianCrawler.sh"
+
+if [ ! -d "../all_data" ]; then
+  mkdir "../all_data"
+fi
+
+if [ ! -d "../all_data/raw_data" ]; then
+  mkdir "../all_data/raw_data"
+fi
+
+if [ ! -d "../all_data/owi_data" ]; then
+  mkdir "../all_data/owi_data"
+fi
+
 source "py_init.sh"
-cd ..
+
 sudo docker compose up -d --build
-#source "insertTest.sh"

590dd734c35397d5eedc6e2a9c7263e5e48376e6
Giancarlo Padoan <giancarlo.p03@gmail.com>
2026-01-14 11:01:33 +0000

    fixed poll

--- scripts/init.sh
+++ scripts/init.sh
@@ -163,7 +163,7 @@ else
 
   # Wait until import completed, and in the meantime print the status
   while true; do
-    STAT=$(curl -s http://localhost:8080/api/v1/importer/status/$JOB_ID_2)
+    STAT_2=$(curl -s http://localhost:8080/api/v1/importer/status/$JOB_ID_2)
     echo "$STAT_2"
 
     if echo "$STAT_2" | grep -q "COMPLETED"; then
